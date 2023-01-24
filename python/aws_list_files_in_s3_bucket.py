aaf868be889f7cbfc4ee974a002f9d6bd822dc4a
jpolkdata <40530465+jpolkdata@users.noreply.github.com>
2023-01-24 09:39:35 -0600

    refactor

--- python/aws_list_files_in_s3_bucket.py
+++ python/aws_list_files_in_s3_bucket.py
@@ -1,33 +1,23 @@
 """
     Given an S3 bucket name, return a list of all file names within that bucket
 """
-
 import os, boto3
 from dotenv import load_dotenv
-load_dotenv() #load environment variables (AWS access key and secret) from local .env file
 
-def get_s3():
-    """Establish an S3 object using AWS credentials that have been specified as 
-    environment variables in a local .env file at the root of the project"""
+if __name__ == '__main__':
+
+    # Loads environment variables (AWS access key and secret) from the local .env file
+    load_dotenv() 
+
+    # Create an S3 resource    
     s3 = boto3.resource(
         service_name='s3',
         region_name='us-east-1',
-        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), #specified in .env file
-        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY") #specified in .env file
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),           #specified in .env file
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")    #specified in .env file
     )
-    return s3
-
-def get_s3_files(bucket_name):
-    """Return a list of all files inside the specified S3 bucket"""
-    bucket_obj = s3.Bucket(bucket_name)
-    file_list = list(bucket_obj.objects.all())
-    return file_list
-
-if __name__ == '__main__':
-    bucket_name='BUCKET_NAME'
-
-    s3 = get_s3()
-
-    files = get_s3_files(bucket_name)
-    for file in files: 
+   
+   # List out the keys (file names) in the data bucket
+    bucket_obj = s3.Bucket('jpolkdata-spotify-data')
+    for file in list(bucket_obj.objects.all()): 
         print(file.key)
