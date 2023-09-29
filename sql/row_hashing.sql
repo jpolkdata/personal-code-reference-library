2f56ad9c49cc175bdbdcaed8abd9b5e06ba35633
jpolkdata <40530465+jpolkdata@users.noreply.github.com>
2023-09-29 14:27:20 -0500

    Added rowhashing note for SQL

--- sql/row_hashing.sql
+++ sql/row_hashing.sql
@@ -11,6 +11,11 @@ CREATE TABLE [dbo].[TestData]
 	resulting data type will be VARBINARY(8000)
 
 	Uppercasing the value ensures that the row hash will not be case sensitive
+
+    You want to add a separator character between columns to allow for empty strings.
+    If you don't do this, the following records are considered the same:
+        'One',",'Another'
+        'One','Another,"
 	*/
     RowHash AS CONVERT(BINARY(64), hashbytes('SHA2_512', CONCAT(
 												UPPER(COALESCE(Name, '')), '|'
