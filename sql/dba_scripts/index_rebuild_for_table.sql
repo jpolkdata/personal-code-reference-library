d9b6685108cb156f89aa2fd0cafdc232501c8115
jpolkdata <40530465+jpolkdata@users.noreply.github.com>
2023-11-23 08:28:45 -0600

    Improved sql index rebuild script, parameterized input values, added logging, added a list of tables instead of just targeting one table

--- sql/dba_scripts/index_rebuild_for_table.sql
+++ sql/dba_scripts/index_rebuild_for_table.sql
@@ -1,46 +1,67 @@
-/**************************************************************************************************************************************************
-	Rebuild all indexes for a given table that have fragmentation over a specific %
-**************************************************************************************************************************************************/
-DECLARE @tableName varchar(100) = 'Receivables'
-DECLARE @indexName varchar(100)
-DECLARE @avgFragmentation float
-DECLARE @sql nvarchar(max)
-DECLARE @msg NVARCHAR(MAX)
+/***************************************************************************************************
+	Rebuild all indexes for a specified set of tables that 	have fragmentation over a specific %
+****************************************************************************************************/
 
+--User parameters
+DECLARE @reorganizeLimit FLOAT = 5; --We'll reorganize indexes above this % but less than the @rebuildLimit
+DECLARE @rebuildLimit FLOAT = 30; --We'll rebuild indexes above this %
+DECLARE @targetTables TABLE (TableName VARCHAR(100)); --We'll define the tables that we want to check below
+INSERT INTO @targetTables (TableName) VALUES ('TABLE1'), ('TABLE2'), ('TABLE3');
+
+--Initialize our working variables
+DECLARE @tableName VARCHAR(100) = '';
+DECLARE @indexName VARCHAR(100) = '';
+DECLARE @avgFragmentation FLOAT = 0;
+DECLARE @sql NVARCHAR(MAX) = '';
+DECLARE @msg NVARCHAR(MAX) = '';
+
+--Create a cursor that contains all the tables we want to target, and their indexes that have some level of fragmentation (> 0%)
 DECLARE ix_cursor CURSOR FOR
 SELECT
-	I.name as [IndexName],
-	DDIPS.avg_fragmentation_in_percent AS 'avgFragmentation'
+	T.[Name] as [TableName]
+	,I.[Name] as [IndexName]
+	,DDIPS.avg_fragmentation_in_percent AS [avgFragmentation]
 FROM sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL, NULL, NULL) AS DDIPS
-INNER JOIN sys.tables T on T.object_id = DDIPS.object_id AND T.name = @tableName
-INNER JOIN sys.indexes I ON I.object_id = DDIPS.object_id
-AND DDIPS.index_id = I.index_id
+JOIN sys.tables T on T.object_id = DDIPS.object_id AND T.[Name] IN (SELECT TableName FROM @targetTables)
+JOIN sys.indexes I ON I.object_id = DDIPS.object_id AND DDIPS.index_id = I.index_id
 WHERE DDIPS.database_id = DB_ID()
-	and I.name is not null
+	and I.[Name] is not null
 	AND DDIPS.avg_fragmentation_in_percent > 0
+ORDER BY T.[Name], I.[Name]
 	
+--For each index, determine if we should REORGANIZE and/or REBUILD based on the fragmentation level
 OPEN ix_cursor
-FETCH NEXT FROM ix_cursor INTO @indexName, @avgFragmentation
+FETCH NEXT FROM ix_cursor INTO @tableName, @indexName, @avgFragmentation
 WHILE @@FETCH_STATUS = 0
 BEGIN
 
-	SET @msg = 'TABLE: ' + @indexName + ' ' + CAST(@avgFragmentation AS varchar(100))
-	RAISERROR (@msg, 0, 1) WITH NOWAIT IF (@avgFragmentation between 5 and 30)
+	--Sanitize the inputs for the table and index name (safeguard against potential SQL injection)
+	SET @tableName = QUOTENAME(@tableName);
+    SET @indexName = QUOTENAME(@indexName);
 
+	--Print information about the index that we are currently processing
+	SET @msg = CONCAT('TABLE: ', @tableName, '; INDEX:', @indexName, '; FRAGMENTATION:', CAST(@avgFragmentation AS VARCHAR(100)));
+	RAISERROR (@msg, 0, 1) WITH NOWAIT;
+
+	--Re-organize the index if the fragmentation is within our threshold
+	IF (@avgFragmentation BETWEEN @reorganizeLimit AND @rebuildLimit)
 	BEGIN
-		SET @sql = 'ALTER INDEX ' + @indexName + ' ON ' + @tableName + ' REORGANIZE';
-		exec sp_executesql @sql;
+		SET @sql = CONCAT('ALTER INDEX ', @indexName, ' ON ', @tableName, ' REORGANIZE');
+		RAISERROR ('...REORGANIZE', 0, 1) WITH NOWAIT;
+		EXEC sp_executesql @sql; 
+	END
+
+	--If the fragmentation level is above the % we specified, then go ahead and rebuild the index
+	IF (@avgFragmentation >= @rebuildLimit)
+	BEGIN
+		SET @sql = CONCAT('ALTER INDEX ', @indexName, ' ON ', @tableName, ' REBUILD');
+		RAISERROR ('...REBUILD', 0, 1) WITH NOWAIT;
+		EXEC sp_executesql @sql;
 	END 
 
-	IF (@avgFragmentation > 30)
-	BEGIN
-		SET @sql = 'ALTER INDEX ' + @indexName + ' ON ' + @tableName + ' REBUILD';
-		exec sp_executesql @sql;
-	END 
-
-FETCH NEXT FROM ix_cursor INTO @indexName, @avgFragmentation
+	--Get the next index to process
+	FETCH NEXT FROM ix_cursor INTO @tableName, @indexName, @avgFragmentation
 END
 
 CLOSE ix_cursor
-DEALLOCATE ix_cursor
-
+DEALLOCATE ix_cursor;
